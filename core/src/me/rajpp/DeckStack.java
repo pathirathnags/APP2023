398b910ee1246f182e8195f45ec1fe16b7e7e43a
Parthsinh Raj <129549278+parthsinhraj@users.noreply.github.com>
2023-04-01 18:56:18 -0400

    Add files via upload

---
+++ core/src/me/rajpp/DeckStack.java
@@ -0,0 +1,66 @@
+package me.rajpp;
+
+import java.util.ArrayList;
+
+public class DeckStack
+{
+    ArrayList<Card> deck;
+    
+    public DeckStack()
+    {
+        deck = new ArrayList<Card>();
+    }
+        
+    public void push(Card value)
+    {
+        deck.add(value);
+    }
+    
+    public Card peek()
+    {
+        return deck.get(deck.size() - 1);
+    }
+    
+    public Card pop()
+    {
+        if(!isEmpty())
+        {
+            Card popValue = deck.get(deck.size() - 1);
+            deck.remove(deck.size - 1);
+            return popValue;
+        }
+        return null;
+    }
+    
+    public Card pop(int index)
+    {
+        if(!isEmpty())
+        {
+            Card popValue = deck.get(index);
+            deck.remove(index);
+            return popValue;
+        }
+        return null;
+    }
+    
+    public boolean isEmpty()
+    {
+        return deck.isEmpty();
+    }
+    
+    public int size()
+    {
+        return deck.size();
+    }
+    
+    public Card get(int index)
+    {
+        return deck.get(index);
+    }
+    
+    
+    
+    
+    
+    
+}
