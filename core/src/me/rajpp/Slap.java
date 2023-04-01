862b7e794b521ef473bfe06702a5a9dc766cb7f4
hendersonong <111789516+hendersonong@users.noreply.github.com>
2023-04-01 19:03:37 -0400

    Update Slap.java

--- core/src/me/rajpp/Slap.java
+++ core/src/me/rajpp/Slap.java
@@ -1,5 +1,8 @@
-
 public interface Slap
 {
-
+ // top bottom, double, sandwich, marriage(queen/king)
+    public boolean topBottom();
+    public boolean doubles();
+    public boolean sandwich();
+    public boolean marriage();
 }
