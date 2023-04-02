edc95bfee31cd4d33775a23858857959b0c9c52d
Parthsinh Raj <129549278+parthsinhraj@users.noreply.github.com>
2023-04-02 05:24:38 -0400

    Delete Slap.java

--- core/src/me/rajpp/Slap.java
+++
@@ -1,8 +0,0 @@
-public interface Slap
-{
- // top bottom, double, sandwich, marriage(queen/king)
-    public boolean topBottom();
-    public boolean doubles();
-    public boolean sandwich();
-    public boolean marriage();
-}
